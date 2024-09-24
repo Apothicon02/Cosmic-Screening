e83c92f79f1cb560f890b719e95d88eb1c7268a8
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-09-24 11:24:35 -0400

    - Added puzzle loader support. - Fixed an incompatibility with mod menu. (Thanks gabe)

--- src/main/java/com/apothicon/cosmicscreening/CosmicScreening.java
+++ src/main/java/com/apothicon/cosmicscreening/CosmicScreening.java
@@ -1,16 +1,5 @@
 package com.apothicon.cosmicscreening;
 
-import dev.crmodders.cosmicquilt.api.entrypoint.ModInitializer;
-import org.quiltmc.loader.api.ModContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class CosmicScreening implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("Cosmic Screening");
+public class CosmicScreening {
 	public static int takeScreenshot = 0;
-
-	@Override
-	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Cosmic Screening Initialized!");
-	}
 }
